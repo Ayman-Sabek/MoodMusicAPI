b6970a4481bd777f9cabfb25108ce6479af69d75
Ayman Sabek <a.sabek313@gmail.com>
2024-07-08 00:28:58 -0400

    Add .gitattributes to handle line endings

--- .platform/hooks/predeploy/01_predeploy.sh
+++ .platform/hooks/predeploy/01_predeploy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /var/app/current
-echo "Setup script running..."
+echo "Predeploy script running..."
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn application:app
+web: gunicorn -w 4 -b 127.0.0.1:8000 application:app
